eed184f63660dd432e2045441bc6bea650704abc
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-09-05 23:03:27 -0300

    Update frela.py

--- frela/frela.py
+++ frela/frela.py
@@ -1,2 +1,13 @@
 # Ler dados da planilha
 # inserir casa élula de cada linha em um campo do sistema
+import openpyxl
+
+workbook = openpyxl.load_workbook('vendas_de_produtos.xlsx')
+sheet_produtos = workbook['vendas']
+
+
+for linha in sheet_produtos.iter_rows(min_row=2):
+    print(linha[0].value)
+    print(linha[1].value)
+    print(linha[2].value)
+    print(linha[3].value)
